2503ef42781fa7f31d3bc1dc44ee40a04cf81192
ahmed <ahmedmahfouzt@gmail.com>
2025-07-08 11:44:53 +0300

    add cidr_private_subnet variable

--- infra/variables.tf
+++ infra/variables.tf
@@ -11,6 +11,12 @@ variable "cidr_public_subnet" {
   type        = list(string)
   description = "Public Subnet CIDR values"
 }
+
+variable "cidr_private_subnet" {
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+}
+
 variable "us_availability_zone" {
   type        = list(string)
   description = "Availability Zones"
