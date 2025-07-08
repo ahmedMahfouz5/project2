cd7cafa6d8adc425e6bb39b57f2e3a3af63e417b
ahmed <ahmedmahfouzt@gmail.com>
2025-07-08 11:44:29 +0300

    add cidr_private_subnet

--- infra/networking/main.tf
+++ infra/networking/main.tf
@@ -2,9 +2,10 @@ variable "vpc_cidr" {}
 variable "vpc_name" {}
 variable "cidr_public_subnet" {}
 variable "us_availability_zone" {}
+variable "cidr_private_subnet" {}
 
 output "vpc_id" {
-  value = aws_vpc.vpc.id
+  value = aws_vpc.vpc-app.id
 }
 
 output "public_subnets" {
@@ -16,7 +17,7 @@ output "public_subnet_cidr_block" {
 }
 
 # Setup VPC
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "vpc-app" {
   cidr_block = var.vpc_cidr
   tags = {
     Name = var.vpc_name
@@ -26,7 +27,7 @@ resource "aws_vpc" "vpc" {
 # Setup public subnet
 resource "aws_subnet" "public_subnets" {
   count             = length(var.cidr_public_subnet)
-  vpc_id            = aws_vpc.vpc.id
+  vpc_id            = aws_vpc.vpc-app.id
   cidr_block        = element(var.cidr_public_subnet, count.index)
   availability_zone = element(var.us_availability_zone, count.index)
 
@@ -38,7 +39,7 @@ resource "aws_subnet" "public_subnets" {
 
 # Setup Internet Gateway
 resource "aws_internet_gateway" "public_internet_gateway" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc-app.id
   tags = {
     Name = "internet_gateway"
   }
@@ -46,7 +47,7 @@ resource "aws_internet_gateway" "public_internet_gateway" {
 
 # Public Route Table
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc-app.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.public_internet_gateway.id
@@ -61,4 +62,32 @@ resource "aws_route_table_association" "public_rt_subnet_association" {
   count          = length(aws_subnet.public_subnets)
   subnet_id      = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.public_route_table.id
+}
+
+# Setup private subnet
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.cidr_private_subnet)
+  vpc_id            = aws_vpc.vpc-app.id
+  cidr_block        = element(var.cidr_private_subnet, count.index)
+  availability_zone = element(var.us_availability_zone, count.index)
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc-app.id
+  #depends_on = [aws_nat_gateway.nat_gateway]
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+# Private Route Table and private Subnet Association
+resource "aws_route_table_association" "private_rt_subnet_association" {
+  count          = length(aws_subnet.private_subnets)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
 }
