9d780023c1a59b0aa23f699c2b6c5da0b2259877
ahmed <ahmedmahfouzt@gmail.com>
2025-07-09 14:46:48 +0300

    template for run flask api

--- infra/template/template.sh
+++ infra/template/template.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
+# shellcheck disable=SC2164
 
-sudo apt update
+cd /home/ubuntu
 
-sudo apt upgrade -y
+# Update system and install necessary packages
+yes | sudo apt update
+yes | sudo apt install python3 python3-pip python3-venv git
 
-sudo apt install apache2 -y
+# Clone the repo
+git clone https://github.com/rahulwagh/python-mysql-db-proj-1.git
+sleep 5
 
-sudo systemctl enable apache2
+cd python-mysql-db-proj-1
 
-sudo systemctl start apache2
+# Create and activate virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies in venv
+pip install -r requirements.txt
+
+echo 'Waiting for 5 seconds before running the app.py'
+sleep 5
+
+# Run app in the background using setsid (detached from terminal)
+setsid python3 app.py &
+
+# Optional: leave venv active or deactivate
+# deactivate
