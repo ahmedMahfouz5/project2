30434b39e00f7e27083727b21636f96c25e8b947
ahmed <ahmedmahfouzt@gmail.com>
2025-07-10 15:15:53 +0300

    allow port 5000

--- infra/security-groups/main.tf
+++ infra/security-groups/main.tf
@@ -34,17 +34,8 @@ resource "aws_security_group" "ec2_sg_ssh_http" {
   ingress {
     description = "Allow HTTP request from anywhere"
     cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-  }
-
-  # enable http
-  ingress {
-    description = "Allow HTTP request from anywhere"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 443
-    to_port     = 443
+    from_port   = 5000
+    to_port     = 5000
     protocol    = "tcp"
   }
 
