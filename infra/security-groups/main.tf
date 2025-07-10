ed9e18aedf5a0a52c81142bfb3c819baa850e5e1
ahmed <ahmedmahfouzt@gmail.com>
2025-07-10 15:41:14 +0300

    delete security_group allow port 8080

--- infra/security-groups/main.tf
+++ infra/security-groups/main.tf
@@ -53,24 +53,6 @@ resource "aws_security_group" "ec2_sg_ssh_http" {
   }
 }
 
-resource "aws_security_group" "ec2_port_8080" {
-  name        = var.ec2_sg_name_http
-  description = "Enable the Port 8080 for jenkins"
-  vpc_id      = var.vpc_id
-
-  # ssh for terraform remote exec
-  ingress {
-    description = "Allow 8080 port to access jenkins"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-  }
-
-  tags = {
-    Name = "Security Groups to allow SSH(22) and HTTP(80)"
-  }
-}
 
 resource "aws_security_group" "lb_allow_port_80" {
   name        = var.ec2_sg_name_http
